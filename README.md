d78a683bf736d4f59db2410cd4c15a62679f599c
zzz <you@example.com>
2015-09-24 14:28:19 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,29 @@
+Yii2 Multiple Path Migration
+============================
+Allow to apply migrations from multiple paths or aliases
+
+Installation
+------------
+via Composer:
+```
+php composer.phar require cyberz/yii2-multipath-migrations ">=1.0.0"
+```
+
+Usage
+-----
+In your console application configuration add:
+
+```php
+'controllerMap' => [
+        'migrate' => [
+            'class' => 'cyberz\migrations\controllers\MigrationsController',
+            'migrationLookup' => [
+                '@app',                         // will look in .../migrations/migration_name.php AND .../<some-dir-name>/migrations/migration_name.php
+                '@backend',                     // will look in .../backend/<some-dir-name>/migrations/migration_name.php AND .../backend/modules/<some-dir-name>/migrations/migration_name.php 
+                '@frontend/modules',            // will look in .../frontend/modules/<some-dir-name>/migrations/migration_name.php
+                '@app/some/path/to/migrations', // will look in .../some/path/to/migrations/migration_name.php
+                '@app/some/*/to/*/migrations',  // will look in .../some/<some-dir-name>/to/<some-dir-name>/migrations
+            ],
+        ],
+    ],
+```
---
+++ src/controllers/MigrationsController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace cyberz\migartions\controllers;
+
+use Yii;
+use yii\console\Exception;
+use yii\console\controllers\MigrateController;
+use yii\helpers\FileHelper;
+
+class MigrationsController extends MigrateController
+{
+
+    /**
+     * @var array aliases of directories
+     */
+    public $migrationLookup = [];
+
+    /**
+     * @var array of early found migrations
+     */
+    private $_cachedLookUp = [];
+
+    /**
+     * Returns the migrations that are not applied.
+     * @return array list of new migrations
+     */
+    protected function getNewMigrations()
+    {
+        $migrations = $this->lookupMigrations();
+
+        foreach ($this->getMigrationHistory(null) as $version => $time) {
+            if (isset($migrations[$version]))
+                unset($migrations[$version]);
+        }
+
+        $migrations = array_keys($migrations);
+        sort($migrations);
+
+        return $migrations;
+    }
+
+    /**
+     * Returns found migrations.
+     * @return array list of migrations and their path
+     */
+    protected function lookupMigrations()
+    {
+
+        if (!empty($this->_cachedLookUp))
+            return $this->_cachedLookUp;
+
+        $this->migrationLookup[] = $this->migrationPath;
+
+        $migrations = [];
+        $ds = DIRECTORY_SEPARATOR;
+
+        foreach ($this->migrationLookup as $migrationPath) {
+
+            $basePath = Yii::getAlias($migrationPath);
+            if (false !== strpos($basePath, '/migrations')) {
+                $patterns[] = $basePath . $ds . '*.php';
+            } elseif (false !== strpos($basePath, '/modules')) {
+                $patterns[] = $basePath . $ds . '*' . $ds . 'migrations' . $ds . '*.php';
+            } else {
+                $patterns[] = $basePath . $ds . '*' . $ds . 'migrations' . $ds . '*.php';
+                $patterns[] = $basePath . $ds . '*' . $ds . 'modules' . $ds . 'migrations' . $ds . '*.php';
+            }
+
+            foreach ($patterns as $pattern) {
+                foreach (glob($pattern) as $path) {
+                    if (preg_match('/^(m(\d{6}_\d{6})_.*?)\.php$/', basename($path), $matches) /*&& !isset($applied[$matches[2]])*/ && is_file($path)) {
+                        $migrations[$matches[1]] = $path;
+                    }
+                }
+            }
+        }
+        $this->_cachedLookUp = $migrations;
+
+        return $migrations;
+    }
+
+    /**
+     * Creates a new migration instance.
+     * @param string $class the migration class name
+     * @return \yii\db\MigrationInterface the migration instance
+     */
+    protected function createMigration($class)
+    {
+        $file = $this->lookupMigrations()[$class];
+        require_once($file);
+
+        return new $class();
+    }
+
+}
